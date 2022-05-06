f083c77e3c94f1d4d52a6e96d1f5a54ec0bac4bf
abschill <ajay198810@gmail.com>
2022-05-06 16:41:38 -0600

    update: type reflection generics

--- lib/config.cc
+++ lib/config.cc
@@ -1,15 +1,9 @@
-#include <iostream>
-#include <filesystem>
-#include "HTMLC.in.h"
-#include <iostream>
-#include <fstream>
-#include <dumb_json/json.hpp>
-#include <filesystem>
 #include "config.hpp"
 using std::filesystem::path;
 using std::filesystem::current_path;
 using std::ifstream;
-dj::json json;
+using std::vector;
+
 namespace fs = std::filesystem;
 
 string file_to_string(const string& path) {
@@ -25,6 +19,7 @@ string file_to_string(const string& path) {
 
 void find_config(string config_path) {
     //todo - config path setup
+    dj::json json;
     fs::path p = current_path();
     fs::path config_file = p / "hcl-config.json";
     string json_string = file_to_string(config_file);
--- lib/config.hpp
+++ lib/config.hpp
@@ -1,7 +1,38 @@
 #include <string>
+#include <iostream>
+#include <filesystem>
+#include "HTMLC.in.h"
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
 using std::string;
+#include <dumb_json/json.hpp>
+
 struct htmlc_config {
     std::string pathRoot;
     std::string partials;
     std::string templates;
 };
+
+template <typename T, typename U>
+struct Converter {
+    T operator()(T const&) const;
+    U operator()(U const&) const;
+};
+
+template <typename T>
+struct Jsonify;
+
+template <typename T>
+    requires dj::json::is_settable<T>
+struct Jsonify<T>: Converter<T, dj::json> {
+    dj::json operator()(T const& t) const { return dj::json(t); }
+    T operator()(dj::json const& json) const { return json.as<T>(); }
+};  
+
+
+template <typename T>
+T fromJson(dj::json const& t) {
+    return Jsonify<T>{}(t);
+}
