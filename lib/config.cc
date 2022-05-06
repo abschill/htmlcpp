f29c4ee21c9099e3c441877fe9b45cc43df041ae
abschill <ajay198810@gmail.com>
2022-05-06 07:22:02 -0600

    update: djson link+include

--- lib/config.cc
+++ lib/config.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <filesystem>
 #include "HTMLC.in.h"
-
+#include <dumb_json/json.hpp>
 
 using std::filesystem::path;
 using std::filesystem::current_path;
 
 void find_config() {
+    path p = current_path();
     std::cout << "Config module (wip)\n";
-    std::cout << "Current Path: " << current_path() << '\n';
+    std::cout << "Current Path: " << p << '\n';
     return;
 }
