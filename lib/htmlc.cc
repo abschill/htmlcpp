d97790fdc4a3ce66227711139562430fd8e16e72
B4S3D <b4s3d11@protonmail.com>
2022-05-07 16:21:50 -0500

    Refactor entire project, create include directory
    C++ standard increased to 23 for <format>

--- lib/htmlc.cc
+++ lib/htmlc.cc
@@ -1,15 +1,22 @@
-#include "HTMLC.in.h"
-#include "config.cc"
-#include "util.cc"
+#include <format>
+#include "htmlc.hh"
+#include "config.hh"
+
+std::pair<std::string, std::string> parse_inline_arg(const std::string& keyval) {
+    auto key = keyval.substr(0, keyval.find("="));
+    // add 1 to the size to offset the delim
+    auto val = keyval.substr(key.size() + 1, keyval.size());
+    return std::make_pair(key, val);
+}
+
 int main(int argc, char *argv[]) {
+    std::cout << std::format("HTMLC v{}.{}\n\n", HTMLC_VERSION_MAJOR, HTMLC_VERSION_MINOR);
     if(argc > 1) {
-        htmlc_config conf = find_config(argv[1]);  
-        println(conf.pathRoot);
-        println(conf.partials);
-        println(conf.templates);
+        auto conf = htmlc::find_config(std::string{ argv[1] });
+        std::cout << std::format("{}\n{}\n{}\n", conf.pathRoot, conf.partials, conf.templates);
     }
     else {
-        println("Enter the path of your htmlc config, or submit inline arguments to parse as key value pairs");
+        std::cout << "Enter the path of your htmlc config.\n";//or submit inline arguments to parse as key value pairs";
     }
 
     return 0;
