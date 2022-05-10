91b48d7854d14e65506b8b1a5e3b8ef7c9df1ff9
abschill <ajay198810@gmail.com>
2022-05-10 12:50:41 -0600

    move to mono

--- lib/htmlc.cpp
+++ lib/htmlc.cpp
@@ -1,11 +1,77 @@
-#include "HTMLC.in.h"
-#include "config.cpp"
-#include "util.cpp"
+#include <HTMLC.h>
+#include "htmlc.hpp"
 #include <fmt/core.h>
+
+namespace htmlc {
+    template <typename T>
+    T fromJson(dj::json const& json) {
+        return json.as<T>;
+    }
+}
+
+namespace htmlc {
+    struct config {
+        std::string pathRoot{};
+        std::string partials{};
+        std::string templates{};
+    };
+
+    string valid_config_path(string c_path) {
+        if(fs::exists(c_path)) {
+            return c_path;
+        }
+        else {
+            throw std::invalid_argument("config path invalid");
+        }
+    }
+    struct info {
+        config config{};
+    };
+
+    template<>
+    config fromJson<config>(dj::json const& config) {
+        return {config["pathRoot"], config["partials"], config["templates"]};
+    }
+
+    template<>
+    info fromJson<info>(dj::json const& info) {
+        return {fromJson<config>(info["config"])};
+    }
+    string path_to_string(const string& path) {
+        ifstream input_file(path);
+        if(!input_file.is_open()) {
+            throw std::invalid_argument("config path invalid");
+        }
+        string json_string = string(std::istreambuf_iterator<char>(input_file), std::istreambuf_iterator<char>());
+        input_file.close();
+        return json_string;
+    }
+
+    
+    htmlc::config find_config(string config_path) {
+        fs::path p = valid_config_path(config_path);
+        dj::json conf;
+        fs::path c_path = p / "htmlc.json";
+        string c_string = path_to_string(c_path);
+        auto res = conf.read(c_string);
+        if(!res) {
+            throw std::invalid_argument("config not defined");
+        }
+        return htmlc::fromJson<htmlc::config>(conf["config"]);
+    }
+
+    // parse a key=value arg from execution context
+    tuple<string, string> parse_inline_arg(string keyval) {
+        string key = keyval.substr(0, keyval.find("="));
+        // add 1 to the size to offset the delim
+        string val = keyval.substr(key.size() + 1, keyval.size());
+        return make_tuple(key, val);
+    }
+}
 
 int main(int argc, char *argv[]) {
     if(argc > 1) {
-        htmlc_config conf = find_config(argv[1]);  
+        htmlc::config conf = htmlc::find_config(argv[1]);  
         fmt::print("root: {}\npartials: {}\ntemplates: {}\n", conf.pathRoot, conf.partials, conf.templates);
     }
     else {
---
+++ lib/htmlc.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <djson/json.hpp>
+using std::tuple;
+using std::make_tuple;
+using std::filesystem::path;
+using std::filesystem::current_path;
+using std::ifstream;
+using std::vector;
+using std::string;
+namespace fs = std::filesystem;
+
