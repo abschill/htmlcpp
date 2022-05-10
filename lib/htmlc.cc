12d98c7ee8930d9214d70988d48501cdb06c6f20
abschill <ajay198810@gmail.com>
2022-05-10 12:02:37 -0600

    add: fmt integration conf print

--- lib/htmlc.cc
+++ lib/htmlc.cc
@@ -1,12 +1,13 @@
 #include "HTMLC.in.h"
 #include "config.cc"
 #include "util.cc"
+#include <fmt/core.h>
+
 int main(int argc, char *argv[]) {
     if(argc > 1) {
         htmlc_config conf = find_config(argv[1]);  
-        println(conf.pathRoot);
-        println(conf.partials);
-        println(conf.templates);
+        std::string conf_string = fmt::format("root: {}\npartials: {}\ntemplates: {}\n", conf.pathRoot, conf.partials, conf.templates);
+        println(conf_string);
     }
     else {
         println("Enter the path of your htmlc config, or submit inline arguments to parse as key value pairs");
