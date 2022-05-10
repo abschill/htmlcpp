3fb26d331a00344f64bb79119cab50830794cf54
abschill <ajay198810@gmail.com>
2022-05-10 12:04:19 -0600

    add: fmt integration conf print

--- lib/htmlc.cc
+++ lib/htmlc.cc
@@ -6,8 +6,7 @@
 int main(int argc, char *argv[]) {
     if(argc > 1) {
         htmlc_config conf = find_config(argv[1]);  
-        std::string conf_string = fmt::format("root: {}\npartials: {}\ntemplates: {}\n", conf.pathRoot, conf.partials, conf.templates);
-        println(conf_string);
+        fmt::print("root: {}\npartials: {}\ntemplates: {}\n", conf.pathRoot, conf.partials, conf.templates);
     }
     else {
         println("Enter the path of your htmlc config, or submit inline arguments to parse as key value pairs");
